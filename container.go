f2efd0b033645ac9ba82f5f1813c841299f98eb7
Ricardo Rossi <rrossi@endata.com>
2015-07-05 09:53:41 -0500

    Cleaned up container

---
+++ container.go
@@ -0,0 +1,62 @@
+package framework
+
+const (
+	LOGGER   = "LOGGER"
+	ROUTER   = "ROUTER"
+	RENDERER = "RENDERER"
+	PARAMS   = "PARAMS"
+	SAMPLE   = "SAMPLE"
+)
+
+var c *Container
+
+type Config map[string]map[string]interface{}
+
+type Container struct {
+	config    Config
+	providers map[string]Providerable
+}
+
+func NewContainer(config ...Config) *Container {
+	c = new(Container)
+	c.providers = map[string]Providerable{}
+	if len(config) > 0 {
+		c.config = config[0]
+	}
+	return c
+}
+
+func (container *Container) RegisterProvider(provider interface{}) {
+	key := provider.(Providerable).GetKey()
+	config := defaultConfig[key]
+	if len(container.config) > 0 {
+		config = container.config[key]
+	}
+	container.providers[key] = provider.(Providerable).Register(config).(Providerable)
+	if key == LOGGER {
+		showLoadingHeader()
+	}
+	container.providers[LOGGER].(Loggable).Info("| * " + key + " ✓")
+}
+
+func showLoadingHeader() {
+	c.providers[LOGGER].(Loggable).Info(`|----------------------------------------|`)
+	c.providers[LOGGER].(Loggable).Info(`| LOADING SERVICE PROVIDERS ...`)
+	c.providers[LOGGER].(Loggable).Info(`|----------------------------------------|`)
+}
+
+func (container *Container) ShowBanner() {
+	log := container.providers[LOGGER].(Loggable).NewLog()
+	log.Info(`|----------------------------------------|`)
+	log.Info(`|    _____								`)
+	log.Info(`|   / ____|           | |					`)
+	log.Info(`|  | |  __  ___  _ __ | |__   ___ _ __	`)
+	log.Info(`|  | | |_ |/ _ \| '_ \| '_ \ / _ \ '__|	`)
+	log.Info(`|  | |__| | (_) | |_) | | | |  __/ |		`)
+	log.Info(`|   \_____|\___/| .__/|_| |_|\___|_|		`)
+	log.Info(`|               | |						`)
+	log.Info(`|               |_|						`)
+	log.Info(`|----------------------------------------|`)
+	log.Info(`| GOPHER READY FOR ACTION ON PORT 3000	`)
+	log.Info(`|----------------------------------------|`)
+}
