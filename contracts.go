1c7b01ebe125aef86ad5ba1d68534409454d298a
Ricardo Rossi <rrossi@endata.com>
2015-07-05 09:51:33 -0500

    Moved contracts to framework repo

---
+++ contracts.go
@@ -0,0 +1,55 @@
+package framework
+
+import (
+	"net/http"
+)
+
+type Providerable interface {
+	Register(config map[string]interface{}) interface{}
+	GetKey() string
+}
+
+type Loggable interface {
+	Providerable
+	NewLog() Loggable
+	Info(msg string, args ...interface{})
+	Debug(msg string, args ...interface{})
+	Warn(msg string, args ...interface{})
+	Error(msg string, args ...interface{})
+	Fatal(msg string, args ...interface{})
+	Panic(msg string, args ...interface{})
+}
+
+type Parametable interface {
+	Providerable
+	PathParams(r *http.Request) map[string]string
+	PathParam(r *http.Request, param string) string
+}
+
+type Renderable interface {
+	Providerable
+	View(rw http.ResponseWriter, status int, name string, binding interface{})
+}
+
+type Routable interface {
+	Providerable
+	http.Handler
+	NewRouter() Routable
+	Get(path string, fn func(http.ResponseWriter, *http.Request))
+	Head(path string, fn func(http.ResponseWriter, *http.Request))
+	Post(path string, fn func(http.ResponseWriter, *http.Request))
+	Put(path string, fn func(http.ResponseWriter, *http.Request))
+	Patch(path string, fn func(http.ResponseWriter, *http.Request))
+	Delete(path string, fn func(http.ResponseWriter, *http.Request))
+	Options(path string, fn func(http.ResponseWriter, *http.Request))
+	Match(path string, fn func(http.ResponseWriter, *http.Request), verbs ...string)
+	All(path string, fn func(http.ResponseWriter, *http.Request))
+	Serve()
+}
+
+type Samplable interface {
+	Providerable
+	NewSample() Samplable
+	GetName() string
+	SetName(name string)
+}
