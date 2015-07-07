bc7a83f4b9f2657738d0e1a95148ea36ceb0df2d
Ricardo Rossi <rrossi@endata.com>
2015-07-07 11:02:11 -0500

    Added routeLoggerMiddleware

---
+++ middleware.go
@@ -0,0 +1,15 @@
+package framework
+
+import "net/http"
+
+type handlerFn func(rw http.ResponseWriter, req *http.Request)
+
+func routeMiddleware(c *Container, rw http.ResponseWriter, req *http.Request, fn handlerFn) {
+	//c.providers[LOGGER].(Loggable).Info("[%s] %s", req.Method, req.URL.Path)
+	fn(rw, req)
+}
+
+func routeLoggerMiddleware(rw http.ResponseWriter, req *http.Request, next func()) {
+	c.providers[LOGGER].(Loggable).Info("[%s] %s", req.Method, req.URL.Path)
+	next()
+}
